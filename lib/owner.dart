ada6074b096d8a423fcc1c7f96bbcb1bcb10f65d
Prawin panta <praveenpanta1082@gmail.com>
2022-02-19 15:37:16 +0545

    Working

--- lib/owner.dart
+++ lib/owner.dart
@@ -10,32 +10,75 @@ class Owner extends StatelessWidget {
       shrinkWrap: true,
       children: [
         Container(
+          // color: Colors.white,
           width: double.infinity,
           height: 100,
+          margin: EdgeInsets.all(5),
+          //width: 200,
           child: ListView(
-            shrinkWrap: true,
+            // shrinkWrap: true,
             scrollDirection: Axis.horizontal,
             children: [
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
-              Text("Hello World"),
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(3)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "House No. 1",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Expanded(
+                      child: Card(
+                        color: Colors.red,
+                        child: Container(
+                          width: double.infinity,
+                          child: Text("Hello world"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("Hello "),
+                ),
+              ),
+              SizedBox(width: 10),
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("Hello "),
+                ),
+              ),
             ],
           ),
         ),
